139bef6eecead368ba393fde668ea7828917eedb
Alex <sashaikonnikov55@gmail.com>
2021-05-26 22:36:13 +0600

    I have not run this.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import './assets/app.scss';
+import { svgSpritePlugin } from 'vue-svg-sprite';
 
-createApp(App).mount('#app');
+createApp(App)
+    .use(svgSpritePlugin, {
+        url: require('./assets/svg/sprite.svg'),
+    })
+    .mount('#app');
